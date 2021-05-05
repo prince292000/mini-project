c784e6e7992a7870882f3e79b700f78b37e821ad
saurabh kumar <technos0ru@gmail.com>
2021-05-06 01:35:08 +0530

    all files are created

---
+++ app.js
@@ -0,0 +1,44 @@
+const ul = document.querySelector("ul");
+const body = document.querySelector("body");
+const h1 = document.querySelector("h1");
+const li = document.querySelector("li");
+
+const length = ul.children.length;
+
+
+    if(length === 1){
+        body.style.background = "white";
+    }
+
+    if(length === 2){
+        body.style.background = "green";
+        ul.style.color = "white";
+        h1.style.color = "white";
+        
+       
+    }
+
+    if(length === 3){
+        body.style.background = "yellow";
+        ul.style.color = "blue";
+        h1.style.color = "blue";
+        
+    }
+
+    if(length === 4){
+        body.style.background = "red";
+        ul.style.color = "white";
+        h1.style.color = "white";
+    }
+
+    if(length === 5){
+        body.style.background = "blue";
+        ul.style.color = "white";
+        h1.style.color = "white";
+    }
+
+    if(length === 6){
+        body.style.background = "red"
+        ul.style.color = "yellow";
+        h1.style.color = "yellow";
+    }
